3f4972d58b008b122b5f39b25427b02183c871a1
未来 栗山 <2410128587@qq.com>
2022-04-15 21:53:50 +0800

    播放页面添加音乐播放、歌词滚动功能

---
+++ QianShi.Music/Common/UserControls/LrcView.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace QianShi.Music.Common.UserControls
+{
+    public class TimeChangedEventArgs : EventArgs
+    {
+        public TimeChangedEventArgs(double time)
+        {
+            Time = time;
+        }
+
+        public double Time { get; set; }
+    }
+
+    /// <summary>
+    /// LrcView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LrcView : UserControl
+    {
+        public Dictionary<double, LrcModel> Lrcs = new Dictionary<double, LrcModel>();
+        //当前焦点所在歌词集合位置
+        public int FoucsLrcLocation { get; set; } = -1;
+        //添加当前焦点歌词变量
+        public LrcModel foucslrc { get; set; }
+        //非焦点歌词颜色
+        public SolidColorBrush NoramlLrcColor = new SolidColorBrush(Colors.Black);
+        //焦点歌词颜色
+        public SolidColorBrush FoucsLrcColor = new SolidColorBrush(Colors.OrangeRed);
+        DispatcherTimer _dt;
+        private bool _rolling = true;
+        public LrcView()
+        {
+            InitializeComponent();
+            _dt = new DispatcherTimer();
+            _dt.Interval = TimeSpan.FromSeconds(5);
+            _dt.Tick += (s, e) =>
+            {
+                _rolling = true;
+                ResetLrcviewScroll();
+            };
+        }
+
+        public event EventHandler<TimeChangedEventArgs> TimeChangedEvent;
+
+        public void LoadLrc(string lyricsString)
+        {
+            lyricsString.Split("\n", StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(lyric =>
+            {
+                var regex = new Regex(@"^\[(\d{2}\:\d{2}\.\d{3})\](.+)$");
+                var match = regex.Match(lyric);
+                if (match.Success)
+                {
+                    var timeStr = match.Groups[1].ToString();
+                    var lyricStr = match.Groups[2].ToString();
+                    var time = TimeSpan.ParseExact(timeStr, @"mm\:ss\.fff", CultureInfo.CurrentCulture).TotalMilliseconds;
+
+                    var textBlock = new TextBlock();
+                    textBlock.Text = lyricStr;
+                    textBlock.FontSize = 28;
+                    textBlock.Opacity = .28;
+                    textBlock.Margin = new Thickness(18);
+                    textBlock.TextWrapping = TextWrapping.WrapWithOverflow;
+                    textBlock.Cursor = Cursors.Hand;
+                    textBlock.MouseLeftButtonUp += (s, e) =>
+                    {
+                        if (TimeChangedEvent != null)
+                        { 
+                            var args = new TimeChangedEventArgs(time);
+                            TimeChangedEvent(this, args);
+                            _dt.Stop();
+                            _rolling = true;
+                            LrcRoll(time);
+                        }
+                    };
+                    var l = new LrcModel
+                    {
+                        LrcTb = textBlock,
+                        LrcText = lyricStr,
+                        Time = time,
+                    };
+                    Lrcs.Add(l.Time, l);
+
+                    LrcItemsControl.Children.Add(textBlock);
+                }
+            });
+        }
+
+        private void ResetLrcviewScroll()
+        {
+            //获得焦点歌词位置
+            GeneralTransform gf = foucslrc.LrcTb.TransformToVisual(LrcItemsControl);
+            Point p = gf.Transform(new Point(0, 0));
+            //滚动条当前位置
+            Debug.WriteLine(ScrollViewerControl.VerticalOffset + "/" + p.Y);
+
+            //计算滚动位置（p.Y是焦点歌词控件(c_LrcTb)相对于父级控件c_lrc_items(StackPanel)的位置）
+            //拿焦点歌词位置减去滚动区域控件高度除以2的值得到的【大概】就是歌词焦点在滚动区域控件的位置
+            double os = p.Y - (ScrollViewerControl.ActualHeight / 2) + 10 + 200;
+            //滚动
+            ScrollViewerControl.ScrollToVerticalOffset(os);
+        }
+
+        public void LrcRoll(double nowTime)
+        {
+            if (foucslrc == null)
+            {
+                foucslrc = Lrcs.Values.First();
+                foucslrc.LrcTb.Foreground = FoucsLrcColor;
+            }
+            else
+            {
+                //查找焦点歌词
+                var s = Lrcs.Where(m => nowTime >= m.Key);
+                if (s.Count() > 0)
+                {
+                    LrcModel lm = s.Last().Value;
+                    foucslrc.LrcTb.Foreground = NoramlLrcColor;
+
+                    foucslrc = lm;
+                    foucslrc.LrcTb.Foreground = FoucsLrcColor;
+                    //定位歌词在控件中间区域
+                    if (_rolling)
+                        ResetLrcviewScroll();
+                }
+            }
+        }
+
+        private void LrcItemsControl_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            _rolling = false;
+            _dt.Stop();
+            _dt.Start();
+        }
+    }
+
+    public class LrcModel
+    {
+        /// <summary>
+        /// 歌词所在控件
+        /// </summary>
+        public TextBlock LrcTb { get; set; }
+
+        /// <summary>
+        /// 歌词字符串
+        /// </summary>
+        public string LrcText { get; set; }
+
+        /// <summary>
+        /// 时间（读取格式参照网易云音乐歌词格式：xx:xx.xx，即分:秒.毫秒，秒小数点保留2位。如：00:28.64）
+        /// </summary>
+        public double Time { get; set; }
+    }
+
+}
--- QianShi.Music/Views/PlayView.xaml.cs
+++ QianShi.Music/Views/PlayView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace QianShi.Music.Views
 {
@@ -21,11 +22,58 @@ namespace QianShi.Music.Views
     public partial class PlayView : UserControl
     {
         private Window _targetWindow = null!;
+        private DispatcherTimer _dt = null!;
+        private bool _playing = false;
+        private bool _canChangProgressControlValue = true;
 
         public PlayView()
         {
             InitializeComponent();
             Loaded += PlayView_Loaded;
+        }
+
+        public void Init(string url, string lyricsString)
+        {
+            LrcView.LoadLrc(lyricsString);
+            LrcView.TimeChangedEvent += (s, e) =>
+            {
+                me.Position = TimeSpan.FromMilliseconds(e.Time);
+                LrcRoll();
+            };
+            _dt = new DispatcherTimer();
+            _dt.Interval = TimeSpan.FromSeconds(0.5);
+            _dt.Tick += (e, c) => LrcRoll();
+            me.Source = new Uri(url);
+            me.LoadedBehavior = MediaState.Pause;
+            me.MediaOpened += (e, c) =>
+            {
+                PlayProgressControl.Maximum = me.NaturalDuration.TimeSpan.TotalMilliseconds;
+                CurrentTimeControl.Text = "00:00";
+                TotalTimeControl.Text = me.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
+                me.LoadedBehavior = MediaState.Manual;
+            };
+            PlayProgressControl.PreviewMouseLeftButtonDown += (e, c) =>
+            {
+                _canChangProgressControlValue = false;
+            };
+            PlayProgressControl.PreviewMouseLeftButtonUp += (e, c) =>
+            {
+                me.Position = TimeSpan.FromMilliseconds(PlayProgressControl.Value);
+                LrcRoll();
+                _canChangProgressControlValue = true;
+            };
+            PlayProgressControl.ValueChanged += (e, c) =>
+            {
+                CurrentTimeControl.Text = me.Position.ToString(@"mm\:ss");
+            };
+        }
+
+        void LrcRoll()
+        {
+            CurrentTimeControl.Text = me.Position.ToString(@"mm\:ss");
+            if (_canChangProgressControlValue)
+                PlayProgressControl.Value = me.Position.TotalMilliseconds;
+            LrcView.LrcRoll(me.Position.TotalMilliseconds);
         }
 
         private void PlayView_Loaded(object sender, RoutedEventArgs e)
@@ -38,6 +86,20 @@ namespace QianShi.Music.Views
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 _targetWindow!.DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_playing)
+            {
+                _dt.Stop();
+                me.Pause();
+            }
+            else
+            {
+                _dt.Start();
+                me.Play();
             }
         }
     }
