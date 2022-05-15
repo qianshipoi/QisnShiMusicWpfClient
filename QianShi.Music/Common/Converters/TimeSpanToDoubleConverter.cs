af208d70cd713ae2b031e7a2f013bb88f9f9c59b
未来 栗山 <2410128587@qq.com>
2022-05-15 20:06:41 +0800

    实现播放控件

---
+++ QianShi.Music/Common/Converters/TimeSpanToDoubleConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace QianShi.Music.Common.Converters;
+
+public class TimeSpanToDoubleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is TimeSpan timeSpan) return timeSpan.TotalMilliseconds;
+        else return 0;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double val) return TimeSpan.FromMilliseconds(val);
+        return TimeSpan.Zero;
+    }
+}
+
+public class DoubleToTimeSpanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is double val) return TimeSpan.FromMilliseconds(val);
+        return TimeSpan.Zero;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is TimeSpan timeSpan) return timeSpan.TotalMilliseconds;
+        return 0;
+    }
+}
--- QianShi.Music/ViewModels/MvViewModel.cs
+++ QianShi.Music/ViewModels/MvViewModel.cs
@@ -7,6 +7,9 @@ using QianShi.Music.Common.Models.Response;
 using QianShi.Music.Services;
 
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace QianShi.Music.ViewModels
 {
@@ -17,10 +20,18 @@ namespace QianShi.Music.ViewModels
 
 
         private readonly IPlaylistService _playlistService;
+        private readonly Dictionary<int, string> _urls = new();
+        private readonly DispatcherTimer _timer = default!;
         private long _mvId;
-        private Dictionary<int, string> _urls = new();
 
-        private string _url = String.Empty;
+        private MediaElement _videoControl = new();
+        public MediaElement VideoControl
+        {
+            get => _videoControl;
+            set => SetProperty(ref _videoControl, value);
+        }
+
+        private string _url = string.Empty;
         public string Url
         {
             get { return _url; }
@@ -41,7 +52,7 @@ namespace QianShi.Music.ViewModels
             set { SetProperty(ref _movieVideos, value); }
         }
 
-        private DelegateCommand<MovieVideo> _playMvCommand;
+        private DelegateCommand<MovieVideo> _playMvCommand = default!;
         public DelegateCommand<MovieVideo> PlayMvCommand =>
             _playMvCommand ?? (_playMvCommand = new DelegateCommand<MovieVideo>(async (mv) =>
             {
@@ -53,12 +64,149 @@ namespace QianShi.Music.ViewModels
                 }
             }));
 
+        private bool _isPlaying = false;
+        public bool IsPlaying
+        {
+            get => _isPlaying;
+            set
+            {
+                if (value == _isPlaying) return;
+                SetProperty(ref _isPlaying, value);
+                if (value)
+                {
+                    _videoControl.Play();
+                    _timer.Start();
+                }
+                else
+                {
+                    _videoControl.Pause();
+                    _timer.Stop();
+                }
+            }
+        }
+
+        private bool _isMuted = false;
+        public bool IsMuted
+        {
+            get => _isMuted;
+            set
+            {
+                SetProperty(ref _isMuted, value);
+                _videoControl.IsMuted = value;
+            }
+        }
+
+        private double _volume = 0.5d;
+        public double Volume
+        {
+            get => _volume;
+            set
+            {
+                SetProperty(ref _volume, value);
+                _videoControl.Volume = value;
+            }
+        }
+
+        private double _position = 0d;
+        public double Position
+        {
+            get => _position;
+            set => SetProperty(ref _position, value);
+        }
+
+        private DelegateCommand _playCommand = default!;
+        public DelegateCommand PlayCommand =>
+            _playCommand ??= new(() =>
+            {
+                if (!IsPlaying)
+                {
+                    IsPlaying = true;
+                    if (ShowCover) ShowCover = false;
+                }
+            });
+
+        private DelegateCommand _pauseCommand = default!;
+        public DelegateCommand PauseCommand =>
+            _pauseCommand ??= new(() =>
+            {
+                if (IsPlaying) IsPlaying = false;
+            });
+
+        private DelegateCommand<bool?> _setMutedCommand = default!;
+        public DelegateCommand<bool?> SetMutedCommand =>
+            _setMutedCommand ??= new(value =>
+            {
+                if (value.HasValue)
+                {
+                    IsMuted = value.Value;
+                }
+            });
+
+        private DelegateCommand<double?> _setPositionCommand = default!;
+        public DelegateCommand<double?> SetPositionCommand =>
+            _setPositionCommand ??= new((value) =>
+            {
+                if (value.HasValue)
+                {
+                    try
+                    {
+                        _timer.Stop();
+                        _videoControl.Position = TimeSpan.FromMilliseconds(value.Value);
+                    }
+                    finally
+                    {
+                        if (IsPlaying)
+                        {
+                            _timer.Start();
+                        }
+                    }
+                }
+            });
+
+        private DelegateCommand<double?> _setVolumeCommand = default!;
+        public DelegateCommand<double?> SetVolumeCommand =>
+            _setVolumeCommand ??= new(value =>
+            {
+                if (value.HasValue)
+                {
+                    Volume = value.Value;
+                }
+            });
+
+        private DelegateCommand<double?> _dragStartedCommand;
+        public DelegateCommand<double?> DragStartedCommand =>
+            _dragStartedCommand ??= new((value) =>
+            {
+                _timer.Stop();
+            });
+
+
+        private bool _showCover = true;
+        public bool ShowCover
+        {
+            get => _showCover;
+            set => SetProperty(ref _showCover, value);
+        }
+
         public MvViewModel(
             IContainerProvider containerProvider,
             IPlaylistService playlistService)
             : base(containerProvider)
         {
             _playlistService = playlistService;
+            _videoControl.Volume = Volume;
+            _videoControl.LoadedBehavior = MediaState.Manual;
+            _videoControl.SetBinding(MediaElement.SourceProperty, nameof(Url));
+
+            _timer = new()
+            {
+                Interval = TimeSpan.FromMilliseconds(200)
+            };
+
+            _timer.Tick += (_, _) =>
+            {
+                Position = _videoControl.Position.TotalMilliseconds;
+            };
         }
 
         public bool KeepAlive => true;
