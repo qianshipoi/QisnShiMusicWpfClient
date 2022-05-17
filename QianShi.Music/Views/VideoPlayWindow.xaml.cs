66554900b15aa633d5b3bb0da12cf666017af568
qianshi <2410128587@qq.com>
2022-05-17 16:52:02 +0800

    新增视频播放窗口

---
+++ QianShi.Music/Views/VideoPlayWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using QianShi.Music.Common.Helpers;
+
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace QianShi.Music.Views
+{
+    /// <summary>
+    /// VideoPlayWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class VideoPlayWindow : Window
+    {
+        public VideoPlayWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SwitchWindowSize(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = (ToggleButton)sender;
+
+            if (toggleButton.IsChecked is true)
+            {
+                FullScreenHelper.StartFullScreen(this);
+            }
+            else
+            {
+                FullScreenHelper.EndFullScreen(this);
+            }
+        }
+
+    }
+}
