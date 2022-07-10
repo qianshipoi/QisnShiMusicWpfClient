340ba15d7da615ed68d1932bbdf19f2ed4ea2695
未来 栗山 <2410128587@qq.com>
2022-07-10 20:12:57 +0800

    added notify property changed attribute.

---
+++ QianShi.Music/Common/Helpers/CacheAttribute.cs
@@ -0,0 +1,13 @@
+﻿
+using Metalama.Framework.Aspects;
+
+namespace QianShi.Music.Common.Helpers
+{
+    public class CacheAttribute : OverrideMethodAspect
+    {
+        public override dynamic? OverrideMethod()
+        {
+            return meta.Proceed();
+        }
+    }
+}
---
+++ QianShi.Music/Common/Helpers/NotifyPropertyChangedAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace QianShi.Music.Common.Helpers
+{
+    public class NotifyPropertyChangedAttribute : TypeAspect
+    {
+        public override void BuildAspect(IAspectBuilder<INamedType> builder)
+        {
+            builder.Advice.ImplementInterface(builder.Target, typeof(INotifyPropertyChanged), whenExists: OverrideStrategy.Ignore);
+
+            var props = builder.Target.Properties.Where(p => !p.IsAbstract && p.Writeability == Writeability.All);
+            foreach (var prop in props)
+            {
+                builder.Advice.OverrideAccessors(prop, null, nameof(this.OverridePropertySetter));
+            }
+        }
+
+        [InterfaceMember]
+        private event PropertyChangedEventHandler? PropertyChanged;
+
+        [Introduce(WhenExists = OverrideStrategy.Ignore)]
+        protected void OnRropertyChanged(string name)
+        {
+            this.PropertyChanged?.Invoke(meta.This, new PropertyChangedEventArgs(name));
+        }
+
+        [Template]
+        private decimal OverridePropertySetter(dynamic value)
+        {
+            if (value != meta.Target.Property.Value)
+            {
+                meta.Proceed();
+                this.OnRropertyChanged(meta.Target.Property.Name);
+            }
+            return value;
+        }
+    }
+}
